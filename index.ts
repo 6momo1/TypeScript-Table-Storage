77531911594593b24059ea1026320722c1696cd6
Tomi Lui <tomilui1221@gmail.com>
2021-03-15 13:18:49 -0700

    Web app complete
    LocalStorage used as a database

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 enum dejiType {
-    Yorkshire = "Yorkshire",
-    Lean = "Lean",
-    Potbelly = "Potbelly"
+    yorkshire = "yorkshire",
+    lean = "lean",
+    potbelly = "potbelly"
 }
 
 enum abilityDescription {
-    Yorkshire = "Water and Ice",
-    Lean = "Fire and Charm",
-    Potbelly = "Electric"
+    yorkshire = "Water and Ice",
+    lean = "Fire and Charm",
+    potbelly = "Electric"
 }
 
 class Dejimon {
@@ -18,12 +18,12 @@ class Dejimon {
     ability: number
     overall: number
     getAbility(){
-        if (this.type == "Yorkshire"){
-            return abilityDescription.Yorkshire
-        } else if (this.type == "Lean"){
-            return abilityDescription.Lean
-        } else if (this.type == "Potbelly"){
-            return abilityDescription.Potbelly
+        if (this.type == "yorkshire"){
+            return abilityDescription.yorkshire
+        } else if (this.type == "lean"){
+            return abilityDescription.lean
+        } else if (this.type == "potbelly"){
+            return abilityDescription.potbelly
         }
     }
     constructor(name:string,t:string,height:number,weight:number,ability:number){
@@ -36,35 +36,59 @@ class Dejimon {
     }
 }
 
-function calcOverall(deji:Dejimon){
-    var overall:number = Math.round((deji.height + deji.weight + deji.ability)/3)
-    console.log(overall)
-}
 
-function searchDejiIndex(name:string){
-    for(var deji in dejimons){
-        if (name == dejimons[deji].name){
-            return deji
-        }
+
+class DejiStorage{
+    //STORAGE MANAGEMENT FOR EASIER USE
+    storage = window.localStorage
+
+    add(deji:Dejimon){
+        //add dejimon to local storage
+        this.storage.setItem(deji.name, JSON.stringify(deji))
+        console.log(`${deji.name} added to storage.`)
     }
-    return -1
+
+    get(name:string){
+        //return Dejimon object from a given name
+        var deji:string = <string>this.storage.getItem(name)
+        console.log(`${name} returned.`)
+        var obj = JSON.parse(deji)
+        var newDeji:Dejimon = new Dejimon(obj.name,obj.type,obj.height,obj.weight,obj.ability)
+        return newDeji
+    }
+    delete(name:string){
+        //delete a Dejimon from a given name
+        this.storage.removeItem(name)
+        console.log(`${name} deleted.`)
+    }
+
+    getAll(){
+        //returns an array of Dejimons
+        var dejimons:Array<Dejimon> = []
+        for (let i = 0; i < this.storage.length; i++) {
+            const key:string = <string>this.storage.key(i)
+            const value:Dejimon = this.get(key)
+            var deji = new Dejimon(value.name,value.type,value.height,value.weight,value.ability)
+            dejimons.push(deji)
+        }
+        return dejimons
+    }
 }
 
-var porkichu = new Dejimon("porkichu",dejiType.Potbelly,170, 60, 90)
-var dogychu = new Dejimon("Doggy",dejiType.Lean,50, 100, 70)
-// calcOverall(porkichu)
-// porkichu.getAbility()
 
-var dejimons:Array<Dejimon> = []
-dejimons.push(porkichu, dogychu)
-// console.log(dejimons)
-// searchDejiIndex("Doggy")
-// console.log(dejimons[1].getAbility())
 
 function updateMainTable(){
-    var table = document.getElementById("dejimons")
-    var s = ''
-    dejimons.forEach(dejimon => {
+    //populate the view table
+    var table:Element = <Element>document.getElementById("dejimons")
+    var s = `<tr>
+                <th scope="col">name</th>
+                <th scope="col">Type</th>
+                <th scope="col"> </th>
+                <th scope="col"> </th>
+            </tr>`
+
+    var dejiArray = dejimons.getAll()
+    dejiArray.forEach(dejimon => {
         s += `
         <tr>
             <td>${dejimon.name}</td>
@@ -74,43 +98,189 @@ function updateMainTable(){
         </tr>
         `
     });
-    table.innerHTML += s
+    table.innerHTML = s
 }
 
-function handleMoreInfo(e){
-    console.log("more info button clicked")
+
+
+
+function handleMoreInfo(e:Element){
+    //update Info Card
+    var name:string = e.target.parentNode.parentNode.children[0].innerHTML
+    var deji = dejimons.get(name)
+    updateInfoTable(deji)
+    displayInfoOnly()
 }
 
-function handleDelete(e){
-    console.log("delete dejimon clicked")
+function handleDelete(e:Element ){
+    var name:string = e.target.parentNode.parentNode.children[0].innerHTML
+    dejimons.delete(name)
+    updateMainTable()
 }
 
-function handleAdd(e){
-    console.log(e.target);
+function handleAdd(e:Element){
+    displayAddOnly()
 }
+
+
+function handleReturn(e:Element){
+    displayViewOnly()
+}
+
+
 
 function updateInfoTable(dejimon:Dejimon){
-    console.log(dejimon)
-    var name:Element | null = document.getElementById("name")
-    var height:Element | null = document.getElementById("height") 
-    var weight:Element | null = document.getElementById("weight")
-    var type:Element | null = document.getElementById("type")
-    var abilityDescription:Element | null = document.getElementById("abilityDescription")
-    var abilityScore:Element | null = document.getElementById("abilityScore")
-    var overall:Element | null = document.getElementById("overall")
+    //Get elements 
+    var name:Element = <Element>document.getElementById("name")
+    var height:Element = <Element>document.getElementById("height") 
+    var weight:Element = <Element>document.getElementById("weight")
+    var type:Element = <Element>document.getElementById("type")
+    var abilityDescription:Element = <Element>document.getElementById("abilityDescription")
+    var abilityScore:Element = <Element>document.getElementById("score")
+    var overall:Element = <Element>document.getElementById("overall")
+
+    //Update the elements
     name.innerHTML = dejimon.name
     height.innerHTML = dejimon.height.toString()
     weight.innerHTML = dejimon.weight.toString()
-    type.innerHTML = dejimon.type
+    type.innerHTML = capitalizeFirstLetter(dejimon.type)
     abilityDescription.innerHTML = dejimon.getAbility() + " Ability"
     abilityScore.innerHTML = dejimon.ability.toString()
     overall.innerHTML = dejimon.overall.toString()
+    console.log(typeof dejimon)
 }
 
-function hideCard(sectionId){
-    var view:Element | null = document.getElementById(sectionId)
+
+
+
+function addDejimon(){
+
+    //Retrieve input from add Dejimon form
+    var name:HTMLInputElement = (<HTMLInputElement>document.getElementById("addName"))
+    var height:HTMLInputElement = (<HTMLInputElement>document.getElementById("addHeight"))
+    var weight:HTMLInputElement = (<HTMLInputElement>document.getElementById("addWeight"))
+    var type:HTMLInputElement = (<HTMLInputElement>document.getElementById("addType"))
+    var ability:HTMLInputElement = (<HTMLInputElement>document.getElementById("addAbility"))    
+    var overall:HTMLInputElement = (<HTMLInputElement>document.getElementById("addOverall"))
+
+
+    //Error handling for invalid text input
+    if (name.value == ""){
+        return alert("Please enter a valid name. ")
+    }
+    if ((height.value == "") || isNaN(parseInt(height.value)) ){
+        return alert("Please enter a valid height. ")
+    }
+    if ((weight.value == "") || isNaN(parseInt(weight.value)) ){
+        return alert("Please enter a valid weight. ")
+    }
+    if ((ability.value == "") || isNaN(parseInt(ability.value)) ){
+        return alert("Please enter a valid ability. ")
+    }
+    if(parseInt(ability.value) > 100 || parseInt(ability.value) < 0){
+        return alert("Please enter an ability value between 0 to 100.")
+    }
+    
+
+
+    //create new Dejimon to add
+    var newDeji = new Dejimon(
+        name.value,
+        type.value,
+        parseInt(height.value),
+        parseInt(weight.value),
+        parseInt(ability.value)
+    )
+
+    //Add dejimon to local array
+    dejimons.add(newDeji)
+
+    //update view table
+    updateMainTable()
+    
+    //reset the input boxes
+    name.value = ""
+    height.value = ""
+    weight.value = ""
+    ability.value = ""
+    overall.innerHTML = "--Dynamic--"
+
+    displayViewOnly()
+}
+
+
+
+function updateAbilityDescription(e:Element){
+    //Dynamically update the ability description in Add Form
+    var type = e.target.value
+    var description:Element | null = <HTMLInputElement>document.getElementById("addDescription")
+    console.log(type, description)
+    if (type == "yorkshire"){
+        description.innerHTML = abilityDescription.yorkshire + " Ability"
+    } else if (type == "lean"){
+        description.innerHTML = abilityDescription.lean + " Ability"
+    } else {
+        description.innerHTML = abilityDescription.potbelly + " Ability"
+    }
+}
+
+//Update the overall score for the add dynamically
+function updateOverallScore(){
+    var height = (<HTMLInputElement>document.getElementById("addHeight")).value
+    var weight = (<HTMLInputElement>document.getElementById("addWeight")).value
+    var ability = (<HTMLInputElement>document.getElementById("addAbility")).value
+    var overall = <HTMLInputElement>document.getElementById("addOverall")
+
+    //Calculate the overall score, then display it on the card
+    var num = Math.round((parseInt(height) + parseInt(weight) + parseInt(ability))/3)
+    overall.innerHTML = num.toString()
+}
+
+
+function capitalizeFirstLetter(string:string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+
+
+function displayViewOnly(){
+    var view:Element = <Element>document.getElementById("viewSection")
+    var add:Element = <Element>document.getElementById("addSection")
+    var info:Element = <Element>document.getElementById("infoSection")
+
+    view.style.display = "block"
+    add.style.display = "none"
+    info.style.display = "none"
+
+    
+}
+
+function displayAddOnly(){
+    var view:Element = <Element>document.getElementById("viewSection")
+    var add:Element = <Element>document.getElementById("addSection")
+    var info:Element = <Element>document.getElementById("infoSection")
+
+    add.style.display = "block"
+    view.style.display = "none"
+    info.style.display = "none"
+}
+
+function displayInfoOnly(){
+    var view:Element = <Element>document.getElementById("viewSection")
+    var add:Element = <Element>document.getElementById("addSection")
+    var info:Element = <Element>document.getElementById("infoSection")
+
+    info.style.display = "block"
+    add.style.display = "none"
     view.style.display = "none"
 }
 
-// updateInfoTable(dejimons[1])
+let dejimons = new DejiStorage()
 updateMainTable()
+
+
+//run the following code to test storage
+// var porkichu = new Dejimon("porkichu",dejiType.potbelly,170, 60, 90)
+// var dogychu = new Dejimon("Doggy",dejiType.lean,50, 100, 70)
+// dejimons.add(porkichu)
+// dejimons.add(dogychu)
